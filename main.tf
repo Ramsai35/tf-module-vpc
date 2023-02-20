96bf497f6623edfdc58cf9ef9d298599a5ff2795
Ramsai35 <mudurukollaramsai335@gmail.com>
2023-02-21 04:42:12 +0530

    Vpc creation

--- main.tf
+++ main.tf
@@ -5,3 +5,14 @@ resource "aws_vpc" "main" {
     { Name = "${var.env}-vpc" }
     )
 }
+
+resource "aws_subnet" "main" {
+  count      = length(var.subnets_cidr)
+  vpc_id     = aws_vpc.main.id
+  cidr_block = var.subnets_cidr[count.index]
+
+  tags       = merge(
+    local.common_tags,
+    { Name = "${var.env}-subnet" }
+  )
+}
