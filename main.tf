59e7747618be2781c149aaacf70d095185752f2e
Ramsai35 <mudurukollaramsai335@gmail.com>
2023-02-21 06:19:27 +0530

    data

--- main.tf
+++ main.tf
@@ -21,4 +21,5 @@ resource "aws_vpc_peering_connection" "peer" {
   peer_owner_id = data.aws_caller_identity.current.account_id
   peer_vpc_id   = "vpc-0b70865c95ef48d55"
   vpc_id        = aws_vpc.main.id
+  auto_accept = true
 }
