386c6893146d7aa428e1b7cf05fa8eb46056ab48
Ramsai35 <mudurukollaramsai335@gmail.com>
2023-02-22 06:56:26 +0530

    route

--- main.tf
+++ main.tf
@@ -64,6 +64,13 @@ resource "aws_route_table" "public" {
   )
   }
 
+resource "aws_route_table_association" "public-rt-assoc" {
+  count          = length(aws_subnet.public)
+  subnet_id      = aws_subnet.public.*.id[count.index]
+  route_table_id = aws_route_table.public.id
+}
+
+
 #resource "aws_route" "default" {
 #  route_table_id            = aws_vpc.main.default_route_table_id
 #  destination_cidr_block    = "172.31.0.0/16"
