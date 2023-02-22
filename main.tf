0a0411ee90b4599eb1e22da4d119113fe83a9b74
Ramsai35 <mudurukollaramsai335@gmail.com>
2023-02-22 07:55:25 +0530

    route

--- main.tf
+++ main.tf
@@ -109,7 +109,7 @@ resource "aws_route_table_association" "private-rt-assoc" {
 
 resource "aws_route" "default" {
   route_table_id            = aws_vpc.main.default_route_table_id
-  destination_cidr_block    = "172.31.0.0/16"
+  destination_cidr_block    = var.cidr_block
   vpc_peering_connection_id = aws_vpc_peering_connection.peer.id
 }
 #resource "aws_route" "igw-route" {
